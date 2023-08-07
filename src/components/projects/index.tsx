5ed245bc0a05ad47bfc2c810dd7ee2a3eca7cdfb
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-08-07 20:46:10 -0300

    feat conect github

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,15 +1,23 @@
 import { Righteous } from 'next/font/google'
 import styles from './styles.module.scss'
+import ProjecstCard from './projectsCard'
+import { api } from '@/services/api';
 
 const righteous = Righteous({ weight: '400', subsets: ['latin'] })
 
-export default function Projects() {
+export default async function Projects() {
+    
+    const response = await api.get('');
+    const projects = response.data;
+
     return (
         <>
             <div id="projetos"></div>
             <section id="projetos" className={styles.container}>
                 <h3 className={righteous.className}>Projetos</h3>
-                <ul></ul>
+                <ul>
+                    {projects.map((project: any, i: any) => <ProjecstCard key={i} project={project} />)}
+                </ul>
             </section>
         </>
 
--- src/components/projects/projectsCard.tsx
+++ src/components/projects/projectsCard.tsx
@@ -1,8 +1,19 @@
+import { FaGithub, FaShare } from 'react-icons/fa'
 import styles from './styles.module.scss'
+interface ProjecstCardProps {
+    project: any
+}
+export default function ProjecstCard({ project }) {
 
-export default function ProjecstCard() {
     return (
         <li className={styles.card}>
+            <h4>{project.name}</h4>
+            <span className={styles.title}>Linguagem:{project.language ? project.language : 'linguagem não definida'}</span>
+            <span className={styles.cat}>Description: {project.description?.substring(0, 129)}</span>
+            <div className={styles.btBox}>
+                <a href={project.html_ur}><FaGithub /> GitHub</a>
+                {project.homepage && <a href={project.homepage}><FaShare />Veja Demo</a>}
+            </div>
         </li>
     )
 }
