ff134beddc2dbca10fb4ff28803f1535b598a932
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-08-07 23:52:03 -0300

    fix card link and styles

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -7,14 +7,14 @@ const righteous = Righteous({ weight: '400', subsets: ['latin'] })
 
 export default async function Projects() {
 
-    const response = await api.get('');
-    const projects = response.data/* [{
+    /* const response = await api.get(''); */
+    const projects = /* response.data */[{
         id: 1,
         name: 'Testando',
         description: "testando testando testando testando testando testando testando testando testando testando",
         language: "Javascript",
-        hrml_ur: "http://localhost"
-    }] */
+        html_url: "https://github.com/project-lets-cook/frontend"
+    }]
 
     return (
         <>
@@ -22,7 +22,7 @@ export default async function Projects() {
             <section id="projetos" className={styles.container}>
                 <h3 className={righteous.className}>Projetos</h3>
                 <ul>
-                    {projects.map((project: any) => <ProjecstCard key={project.id} project={project} />)}
+                    {projects ? projects.map((project: any) => <ProjecstCard key={project.id} project={project} />) : <span>Algo deu errado! :(</span>}
                 </ul>
             </section>
         </>
--- src/components/projects/projectsCard.tsx
+++ src/components/projects/projectsCard.tsx
@@ -8,10 +8,10 @@ export default function ProjecstCard({ project }: ProjecstCardProps) {
     return (
         <li className={styles.card}>
             <h4 className={styles.title}>{project.name}</h4>
-            <span className={styles.language}>Linguagem:{project.language ? project.language : 'não definida'}</span>
+            <span className={styles.language}>Linguagem:{project.language ? project.language : 'linguagem não definida'}</span>
             <span className={styles.descripton}>Description: {project.description?.substring(0, 129)}</span>
             <div className={styles.btBox}>
-                <a href={project.html_ur} target='blank'><FaGithub /> GitHub</a>
+                <a href={project.html_url} target='blank'><FaGithub /> GitHub</a>
                 {project.homepage && <a href={project.homepage} target='blank'><FaShare />Veja Demo</a>}
             </div>
         </li>
